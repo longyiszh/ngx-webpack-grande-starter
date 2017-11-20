6a1ca5d47bc19aa9e12f9a6abdb64a139ef316ad
wcxaaa <wcx@xtoneict.com>
2017-11-20 14:58:01 +0800

    + dummy module

---
+++ src/client/app/dummy/dummy.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-dummy',
+	templateUrl: './dummy.component.html',
+	styleUrls: ['./dummy.component.css']
+})
+export class DummyComponent implements OnInit {
+	constructor(
+	) { }
+	
+	ngOnInit() {}
+
+}
